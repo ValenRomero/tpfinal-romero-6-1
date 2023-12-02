ae9010b9210ab263f9f413f601dbfe4f013a77ad
Aguss <agussx17x@gmail.com>
2023-12-02 20:11:14 -0300

    cambios en catalogo y servicio.

--- src/app/modules/inicio/pages/catalogo/catalogo.component.ts
+++ src/app/modules/inicio/pages/catalogo/catalogo.component.ts
@@ -1,23 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/modules/services/product.service';
+
+
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
-  styleUrls: ['./catalogo.component.css'], 
+  styleUrls: ['./catalogo.component.css']
 })
 export class CatalogoComponent implements OnInit {
   products: Product[] = [];
+  productToView: Product = {
+    id: '',
+    title: '',
+    description: '',
+    price: 0,
+    imageUrl: ''
+  };
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
     this.productService.getProducts().subscribe(data => {
       this.products = data.map(e => {
-        return {
-          ...e.payload.doc.data() as Product
-        } as Product;
+        let product = e.payload.doc.data() as Product;
+        product.id = e.payload.doc.id;
+        return product;
       })
     });
+  }
+
+  openViewModal(product: Product) {
+    this.productToView = {...product};
   }
 }
